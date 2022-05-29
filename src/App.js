4387c341a5de21654f08a613f65faec0b3625e26
stobertonf <stobertonf@gmail.com>
2022-05-29 18:38:53 -0300

    feat(Profile): Learning to style in React.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import {View, Text} from 'react-dom';
 
 const App = () => {
     return (
-    <View>
+    <View style={style.container}>
         <Text>Hello World</Text>
     </View>
     );
 };
 
 export default App;
+
+const style = StyleSheet.create({
+    container: {
+        backgroundColor:'black',
+        flex: 1, //Expande para a tela inteira
+    },
+    text: {
+        fontSize: 50,
+        fontWeight: 'bold',
+    },
+});
